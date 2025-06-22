ebe34fa49da20bf4c45ab95adfc951f5791ad4fc
viannik <ivan.nikitski@gmail.com>
2025-06-23 00:47:10 +0300

    Implement Person class with spouse relationship management

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
+
+    def set_spouse(self, relation: str, partner: "Person") -> None:
+        if not hasattr(self, relation):
+            setattr(self, relation, partner)
+            setattr(partner, "husband" if relation == "wife" else "wife", self)
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = [Person(person["name"], person["age"]) for person in people]
+
+    for person in people:
+        current_person = Person.people[person["name"]]
+        for relation in ("wife", "husband"):
+            partner_name = person.get(relation)
+            if partner_name:
+                partner = Person.people[partner_name]
+                current_person.set_spouse(relation, partner)
+
+    return person_list
